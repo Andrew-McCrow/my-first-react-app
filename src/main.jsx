29e1319e0c47bbe42390a9d7bcd3508ce1c88317
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-03 21:29:56 +0000

    react set-up pt 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Greeting from './Greeting.jsx'
+
+const element = <Greeting />;
+
+console.log(element);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
